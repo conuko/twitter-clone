23404807cc8c081b1721d5b2bee39b8a435be320
Constantin Unterkofler <constantinu93@gmail.com>
2021-02-11 18:06:11 +0100

    feat: add react-redux-loading

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
+import LoadingBar from 'react-redux-loading';
 
 
 class App extends Component {
@@ -11,7 +12,8 @@ class App extends Component {
   render() {
     return (
       <div>
-      {/* if loading is still true, nothing will be rendered. Else, the Dashboard will be rendered: */}
+        <LoadingBar />
+        {/* if loading is still true, nothing will be rendered. Else, the Dashboard will be rendered: */}
         {this.props.loading === true
           ? null
           : <Dashboard />
