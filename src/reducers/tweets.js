05ccc3a1b977c6e9726ae2c822e6a6996f93a30b
Constantin Unterkofler <constantinu93@gmail.com>
2021-02-14 13:06:31 +0100

    feat: add ADD_TWEET action type to reducer

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,4 @@
-import { RECEIVE_TWEETS, TOGGLE_TWEET } from '../actions/tweets';
+import { RECEIVE_TWEETS, TOGGLE_TWEET, ADD_TWEET } from '../actions/tweets';
 
 export default function tweets (state = {}, action) {
     switch(action.type) {
@@ -18,8 +18,30 @@ export default function tweets (state = {}, action) {
                         // if they haven't liked the tweet, we add the user name of the authenticated user to the likes array:
                         : state[action.id].likes.concat([action.authedUser])
                 }
-
             }
+        case ADD_TWEET :
+            const { tweet } = action;
+            const replyingTo = {};
+
+            // if the new added tweet is a reply to another tweet, we want to grab that new tweets,
+            // spread all of the previous properties onto that new tweet, concatenate onto the replies array
+            // the new reply that we just created and then we want to spread this whole object onto our tweets array,
+            // so it will update this specific portion of our state:
+            if (tweet.replyingTo !== null) {
+                replyingTo = {
+                    [tweet.replyingTo]: {
+                        ...state[tweet.replyingTo],
+                        replies: state[tweet.replyTo].replies.concat([tweet.id])
+                    }
+                };
+            };
+            // when we add a new tweet, we take that tweet and add it to our tweets slice of our state
+            return {
+                ...state,
+                [action.tweet.id]: action.tweet,
+                ...replyingTo,
+            };
+
         default :
             return state
     }
