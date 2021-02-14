32660a771d89b8895180c7e409ae016b672c4e6f
Constantin Unterkofler <constantinu93@gmail.com>
2021-02-14 14:16:14 +0100

    feat: create Navbar component and add React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
 import NewTweet from './NewTweet';
 import TweetPage from './TweetPage';
+import Navbar from './Navbar';
 
 
 class App extends Component {
@@ -13,14 +15,23 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <LoadingBar />
-        {/* if loading is still true, nothing will be rendered. Else, the Dashboard will be rendered: */}
-        {this.props.loading === true
-          ? null
-          : <TweetPage match={{params: {id: '6h5ims9iks66d4m7kqizmv'}}}/>
-        }
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className='container'>
+              <Navbar />
+              {/* if loading is still true, nothing will be rendered. Else, the Dashboard will be rendered: */}
+              {this.props.loading === true
+                ? null
+                : <div>
+                    <Route path='/' exact component={Dashboard} />
+                    <Route path='/tweet/:id' component={TweetPage} />
+                    <Route path='/new' component={NewTweet} />
+                </div>}
+          </div>
+        </Fragment>
+      </Router>
+
     )
   }
 }
